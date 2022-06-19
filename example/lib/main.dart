87d09870c9fde13ac14fadc95af149101fe09eaf
Zero <1300326388@qq.com>
2022-06-19 18:34:47 +0800

    1、Add example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,60 +1,61 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_close_app/flutter_close_app.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await FlutterCloseApp.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return FlutterCloseAppPage(
+      interval: 2,
+      onCloseFailed: () {
+        // The interval is more than 2 seconds, or the return key is pressed for the first time
+        print('onCloseFailed');
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Press again to exit 🎉'),
+        ));
+      },
+      child: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Flutter Close App'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const ListTile(
+                leading: Icon(
+                  Icons.android,
+                  color: Colors.green,
+                ),
+                title: Text('Double back to exit app'),
+              ),
+              const Divider(),
+              ListTile(
+                leading: const Icon(
+                  Icons.exit_to_app,
+                  color: Colors.red,
+                ),
+                title: const Text('Tap close app'),
+                onTap: () {
+                  FlutterCloseApp.close();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/flutter_close_app.dart
+++ lib/flutter_close_app.dart
@@ -1,13 +1,59 @@
+import 'dart:io';
 
-import 'dart:async';
+import 'package:flutter/widgets.dart';
 
-import 'package:flutter/services.dart';
+/// Flutter Close App Page
+class FlutterCloseAppPage extends StatefulWidget {
+  const FlutterCloseAppPage({
+    Key? key,
+    required this.child,
+    this.interval = 2,
+    this.onCloseFailed,
+  }) : super(key: key);
 
+  /// The widget to wrap.
+  final Widget child;
+
+  /// The interval in seconds to close the app.
+  final int interval;
+
+  /// The callback when the condition is failed.
+  final VoidCallback? onCloseFailed;
+
+  @override
+  State<FlutterCloseAppPage> createState() => _FlutterCloseAppPageState();
+}
+
+class _FlutterCloseAppPageState extends State<FlutterCloseAppPage> {
+  int lastTime = 0;
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (!Platform.isAndroid) {
+          return true;
+        }
+        int diff = DateTime.now().second - lastTime;
+        if (diff >= 0 && diff <= widget.interval) {
+          exit(0);
+        } else {
+          lastTime = DateTime.now().second;
+          widget.onCloseFailed?.call();
+          return false;
+        }
+      },
+      child: widget.child,
+    );
+  }
+}
+
+/// Flutter Close App
 class FlutterCloseApp {
-  static const MethodChannel _channel = MethodChannel('flutter_close_app');
-
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  /// Close the app
+  /// ```
+  /// FlutterCloseApp.close();
+  /// ```
+  static void close() {
+    exit(0);
   }
 }
